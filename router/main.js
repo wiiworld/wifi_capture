ad6c34bc7acc1eebd42eea1c5a07df820e0562af
wiiworld <xile803@likelion.org>
2020-09-18 00:05:50 +0900

    add js

---
+++ router/main.js
@@ -0,0 +1,1 @@
+var express = require('express')var router = express.Router();var bodyParser = require('body-parser')router.use(bodyParser.urlencoded({extended:false}))router.get('/', function(req, res){    res.render('index.html')});module.exports = router;
